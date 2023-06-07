a08634e6e74c5caa9fddf51286201c406190d472
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-06-07 17:41:11 +0200

    setting nel construttore delle eccezioni,non so se sia giusto

--- src/org/lessons/java/library/Book.java
+++ src/org/lessons/java/library/Book.java
@@ -1,4 +1,81 @@
 package org.lessons.java.library;
 
 public class Book {
+    //Fields
+    private String titolo;
+    private int numeroPagine;
+    private String autore;
+    private String editore;
+    //Constructor
+//    le gestione delle validazioni teoricamente dovrei affidarla al costruttore tramite i metodi setter di ogni attributo
+    public Book (String titolo, int numeroPagine, String autore, String editore) {
+
+        if (titolo.isEmpty()) {
+            throw new RuntimeException();
+        } else {
+            this.titolo = titolo;
+        }
+        if (numeroPagine <= 0) {
+            throw new RuntimeException();
+
+        } else {
+            this.numeroPagine = numeroPagine;
+        }
+        if (autore.isEmpty()) {
+            throw new RuntimeException();
+
+        } else {
+            this.autore = autore;
+        }
+        if (editore.isEmpty()) {
+            throw new RuntimeException();
+
+        } else {
+            this.editore = editore;
+        }
+    }
+    //Getters and Setters
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public void setTitolo(String titolo) {
+        this.titolo = titolo;
+    }
+
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
+
+    public void setNumeroPagine(int numeroPagine) {
+        this.numeroPagine = numeroPagine;
+    }
+
+    public String getAutore() {
+        return autore;
+    }
+
+    public void setAutore(String autore) {
+        this.autore = autore;
+    }
+
+    public String getEditore() {
+        return editore;
+    }
+
+    public void setEditore(String editore) {
+        this.editore = editore;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "titolo='" + titolo + '\'' +
+                ", numeroPagine=" + numeroPagine +
+                ", autore='" + autore + '\'' +
+                ", editore='" + editore + '\'' +
+                '}';
+    }
 }
--- src/org/lessons/java/library/Main.java
+++ src/org/lessons/java/library/Main.java
@@ -1,7 +1,41 @@
 package org.lessons.java.library;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+//        creo l'array'
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Quanti libri vuoi aggiungere alla tua libreria personale? da 1 a 5...");
+        int nBooks = Integer.parseInt(scanner.nextLine());
+        Book[] personalLibrary = new Book[nBooks];
+
+//        creo un ciclo che continuera' fino alla fine dell'array stabilito dall'utente'
+
+        for (int i = 0; i < personalLibrary.length; i++) {
+            System.out.println("titolo del libro numero " + (i + 1) + ": ");
+            String titolo = scanner.nextLine();
+            System.out.println("numero di pagine: ");
+            int numeroPagine = Integer.parseInt(scanner.nextLine());
+            System.out.println("autore: ");
+            String autore = scanner.nextLine();
+            System.out.println("editore: ");
+            String editore = scanner.nextLine();
+               try {
+//                   provo ad ottenere questo risultato salvo la cattura di errori
+                   Book book = new Book(titolo, numeroPagine,autore, editore);
+                   personalLibrary[i] = book;
+               }
+            catch(RuntimeException e) {
+                System.out.println("i parametri inseriti per i tuoi libri non sono corretti,riprova");i--;
+            }
+
+            }
+        System.out.println(Arrays.toString(personalLibrary));
+        scanner.close();
+
     }
+
 }
